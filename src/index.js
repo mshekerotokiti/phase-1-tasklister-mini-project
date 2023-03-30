b0775bfff5cf3c4a55a3cc2217794b401b759bce
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-30 17:46:43 +0300

    completed challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const createToDo = document.getElementById("new-task-description").value;
+    const li = document.createElement("li");
+    li.textContent = createToDo;
+document.querySelector("#tasks").appendChild(li);
+document.querySelector("form").reset()
+
+  })
 });
+
+// function create(todo){
+//   let p = document.createElement('p')
+//   let button = document.createElement("button")
+//   button.textContent = 'delete'
+//   button.addEventListener('click', handleDelete)
+//   console.log(p)
+//   p.textContent = `${todo} `
+//   document.getElementById("tasks").appendChild(p)
+//   p.appendChild(button)
+// }
+
+// function handleDelete(event){
+//   event.target.parentNode.remove()
+// }
